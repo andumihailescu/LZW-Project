d02ade1c9b92bc86b33b708d44aced39f219e82b
andumihailescu <contact.andumihailescu@gmail.com>
2023-11-02 20:28:29 +0200

    Created the the encoder and decoder classes
    - The encoder is almost working

---
+++ LZW/Encoder.cs
@@ -0,0 +1,81 @@
+﻿using Bit_Reader_Writer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZW
+{
+    internal class Encoder
+    {
+        private BitReader bitReader;
+        private BitWriter bitWriter;
+        private long fileLength;
+        private long fileLengthInBits;
+        private string encoderInputFilePath;
+        private string encoderOutputFilePath;
+        private List<string> symbolsDictionary;
+        private int indicesSizeInBits;
+
+        public Encoder()
+        {
+
+        }
+
+        public void InitialiseEncoder(string inputFilePath, string outputFilePath)
+        {
+            bitReader = new BitReader(inputFilePath);
+            bitWriter = new BitWriter(outputFilePath);
+            fileLength = bitReader.GetFileLengthInBytes();
+            fileLengthInBits = fileLength * 8;
+
+            symbolsDictionary = new List<string>();
+            for (int i = 0; i < 256; i++)
+            {
+                char symbol = Convert.ToChar(i);
+                symbolsDictionary.Add(symbol.ToString());
+            }
+
+            encoderInputFilePath = inputFilePath;
+            encoderOutputFilePath = outputFilePath;
+        }
+
+        public void SetIndicesSize(int size)
+        {
+            indicesSizeInBits = size;
+        }
+
+        public void EncodeFile()
+        {
+            int symbolSize = 8;
+            long numberOfRemainingBits = fileLengthInBits;
+
+            string s = string.Empty; char ch;
+
+            do
+            {
+                uint value = bitReader.ReadNBits(symbolSize);
+                ch = (char)value;
+                if (symbolsDictionary.Contains(s + ch)){
+                    s = s + ch;
+                }
+                else
+                {
+                    //mai jos, in loc de s, trebuie scris indexul de unde pleaca s
+                    bitWriter.WriteNBits(indicesSizeInBits, Convert.ToUInt32(s));
+                    symbolsDictionary.Add(s + ch);
+                    s = "" + ch;
+                }
+                numberOfRemainingBits -= symbolSize;
+
+            } while (numberOfRemainingBits > 0);
+
+            bitReader.CloseFile();
+            bitWriter.CloseFile();
+
+        }
+    }
+}
--- LZW/Form1.cs
+++ LZW/Form1.cs
@@ -1,7 +1,17 @@
+using System.Drawing.Imaging;
+
 namespace LZW
 {
     public partial class Form1 : Form
     {
+        private Encoder encoder;
+        private Decoder decoder;
+        private string encoderInputFilePath;
+        private string encoderOutputFilePath;
+        private string decoderInputFilePath;
+        private string decoderOutputFilePath;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -9,21 +19,58 @@ namespace LZW
 
         private void loadOriginalFileBtn_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = @"D:\School\CIM\LZW\Files";
+            openFileDialog.ShowDialog();
+            encoderInputFilePath = openFileDialog.FileName;
+            encoderOutputFilePath = encoderInputFilePath;
 
+            //display the file path
+
+            encoder = new Encoder();
         }
 
         private void encodeFileBtn_Click(object sender, EventArgs e)
         {
+            encoderOutputFilePath += ".";
+            if (chooseDictionaryTypeCb.Text == "Freeze")
+            {
+                encoderOutputFilePath += "f";
+            }
+            else
+            {
+                encoderOutputFilePath += "e";
+            }
+
+            encoderOutputFilePath += chooseNumberOfBitsCb.Text;
+
+            encoderOutputFilePath += ".lzw";
+
+            encoder.InitialiseEncoder(encoderInputFilePath, encoderOutputFilePath);
+            encoder.SetIndicesSize(Convert.ToInt32(chooseNumberOfBitsCb.Text));
+            encoder.EncodeFile();
 
         }
 
         private void loadEncodedFileBtn_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = @"D:\School\CIM\LZW\Files";
+            openFileDialog.Filter = "Text Files|*.lzw|All Files|*.*";
+            openFileDialog.ShowDialog();
+            decoderInputFilePath = openFileDialog.FileName;
 
+            string[] parts = decoderInputFilePath.Split('.');
+            string extension = parts[parts.Length - 2];
+
+            decoderOutputFilePath = decoderInputFilePath + "." + extension;
+
+            decoder = new Decoder();
         }
 
         private void decodeFileBtn_Click(object sender, EventArgs e)
         {
+
 
         }
     }
